c1e088e1ac2f50b2070da8d36645a545bb71bff3
Umair Shaikh <umairjr1265@gmail.com>
2024-02-24 02:53:41 +0530

    First commit

---
+++ pages/components/Navigate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarUI from './ui/Navbar';
+
+const Navigate = ({ accounts, web3Handler }) => {
+
+  return (
+    <>
+      const title = "RealEstate"
+      <NavbarUI title={title} web3Handler={web3Handler} accounts={accounts} />
+    </>
+  );
+};
+
+export default Navigate;
